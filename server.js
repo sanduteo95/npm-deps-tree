691c22e444edaeb296adb8a09f709625e7dbc6b8
Teodora Sandu <tas458@cs.bham.ac.uk>
2021-02-20 18:31:28 +0000

    handle exit

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const app = require('./app')
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`)
 })
+
+const handleExit = (signal) => {
+  console.log(`Received ${signal}. Close my server properly.`)
+  // @ts-ignore
+  server.close(() => {
+    process.exit(0)
+  })
+}
+
+process.on('SIGINT', handleExit)
+process.on('SIGQUIT', handleExit)
+process.on('SIGTERM', handleExit)
